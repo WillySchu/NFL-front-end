c96b6c555041c6535c0aa5f2df22622676ee8a2a
Will Schumacher <meintodengel@gmail.com>
2016-06-30 15:25:27 -0700

    refactor features into single object

--- controllers/main.js
+++ controllers/main.js
@@ -6,8 +6,9 @@ Main.$inject = ['Prediction', '$state']
 function Main(Prediction, $state) {
   const vm = this;
 
-  vm.submit = function(down, ydstogo, yrdline100, ScoreDiff, TimeSecs) {
-    Prediction.submit(down, ydstogo, yrdline100, ScoreDiff, TimeSecs).then(function(result) {
+  vm.submit = function(features) {
+    console.log(features);
+    Prediction.submit(features).then(function(result) {
       vm.result = JSON.parse(result);
       $state.go('main.result');
     })
--- services/main.js
+++ services/main.js
@@ -2,8 +2,8 @@ angular.module('app')
   .factory('Prediction', function($http) {
     const baseUrl = 'http://0.0.0.0:5000/'
     return {
-      submit: function(down, ydstogo, yrdline100, ScoreDiff, TimeSecs) {
-        return $http.post(baseUrl, {down, ydstogo, yrdline100, ScoreDiff, TimeSecs}).then(function(data) {
+      submit: function(features) {
+        return $http.post(baseUrl, features).then(function(data) {
           return data.data
         })
       }
