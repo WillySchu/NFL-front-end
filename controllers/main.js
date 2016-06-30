2c99441c6e2f392fd3d9784bf5a6d1ae1d71ed9b
Will Schumacher <meintodengel@gmail.com>
2016-06-30 15:21:34 -0700

    add yrdline100 input and variable to call

--- controllers/main.js
+++ controllers/main.js
@@ -6,9 +6,8 @@ Main.$inject = ['Prediction', '$state']
 function Main(Prediction, $state) {
   const vm = this;
 
-  vm.submit = function(down, ydstogo, ScoreDiff, TimeSecs) {
-    Prediction.submit(down, ydstogo, ScoreDiff, TimeSecs).then(function(result) {
-      console.log(JSON.parse(result));
+  vm.submit = function(down, ydstogo, yrdline100, ScoreDiff, TimeSecs) {
+    Prediction.submit(down, ydstogo, yrdline100, ScoreDiff, TimeSecs).then(function(result) {
       vm.result = JSON.parse(result);
       $state.go('main.result');
     })
--- services/main.js
+++ services/main.js
@@ -2,8 +2,8 @@ angular.module('app')
   .factory('Prediction', function($http) {
     const baseUrl = 'http://0.0.0.0:5000/'
     return {
-      submit: function(down, ydstogo, ScoreDiff, TimeSecs) {
-        return $http.post(baseUrl, {down, ydstogo, ScoreDiff, TimeSecs}).then(function(data) {
+      submit: function(down, ydstogo, yrdline100, ScoreDiff, TimeSecs) {
+        return $http.post(baseUrl, {down, ydstogo, yrdline100, ScoreDiff, TimeSecs}).then(function(data) {
           return data.data
         })
       }
