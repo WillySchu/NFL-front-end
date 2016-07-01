0dc050ce394b859b1a960ec021b4ea1858aeca4f
Will Schumacher <meintodengel@gmail.com>
2016-06-30 17:16:47 -0700

    add urls for dev, stage and pro

--- services/main.js
+++ services/main.js
@@ -1,10 +1,12 @@
 angular.module('app')
   .factory('Prediction', function($http) {
-    const baseUrl = 'http://0.0.0.0:5000/'
+    const devUrl = 'http://0.0.0.0:5000/';
+    const stageUrl = 'https://nfl-playbyplay-stage.herokuapp.com/';
+    const proUrl = 'https://nfl-playbyplay-pro.herokuapp.com/';
     return {
       submit: function(features) {
-        return $http.post(baseUrl, features).then(function(data) {
-          return data.data
+        return $http.post(stageUrl, features).then(function(data) {
+          return data.data;
         })
       }
     }
