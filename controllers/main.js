e392aeaa87b54582eaf328524685f26e3e2daccc
Will Schumacher <meintodengel@gmail.com>
2016-07-04 12:33:43 -0700

    functional zoomable sunburst chart

--- controllers/main.js
+++ controllers/main.js
@@ -9,47 +9,33 @@ function Main(Prediction, $state) {
   vm.submit = function(features) {
     Prediction.submit(features).then(function(result) {
       vm.result = JSON.parse(result);
-
       fResult = []
       names = ['Pass', 'Run', 'Punt', 'Field Goal']
 
       for (i in vm.result) {
         vm.result[i]
 
-        fResult.push({'key': names.shift(), 'y': vm.result[i]})
+        fResult.push({'name': names.shift(), 'size': vm.result[i]})
       }
 
-      console.log(fResult);
-
-      vm.data = fResult;
+      vm.data = [{
+        name: 'chart',
+        children: fResult
+      }]
 
       $state.go('main.result');
     })
   }
 
+  console.log(d3);
 
   vm.options = {
     chart: {
-      type: 'pieChart',
+      type: 'sunburstChart',
       height: 450,
-      donut: true,
-      x: function(d){return d.key;},
-      y: function(d){return d.y;},
-      showLabels: true,
-
-      pie: {
-        startAngle: function(d) { return d.startAngle/2 -Math.PI/2 },
-        endAngle: function(d) { return d.endAngle/2 -Math.PI/2 }
-      },
-      duration: 500,
-      legend: {
-        margin: {
-          top: 5,
-          right: 70,
-          bottom: 5,
-          left: 0
-        }
-      }
+      color: d3.scale.category10(),
+      duration: 250,
+      mode: 'size'
     }
   }
 }
