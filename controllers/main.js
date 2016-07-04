2e705eeae44442eabe35b18a0304b6fdf02da82f
Will Schumacher <meintodengel@gmail.com>
2016-07-04 15:09:03 -0700

    format controller to display types of runs and passes

--- controllers/main.js
+++ controllers/main.js
@@ -9,13 +9,23 @@ function Main(Prediction, $state) {
   vm.submit = function(features) {
     Prediction.submit(features).then(function(result) {
       vm.result = JSON.parse(result);
-      fResult = []
-      names = ['Pass', 'Run', 'Punt', 'Field Goal']
+      fResult = [
+        {name: 'Pass', children: []},
+        {name: 'Run', children: []}
+      ]
+
+      names = ['Pass', 'Pass, Short Right', 'Pass, Short Middle', 'Pass, Short Left', 'Pass, Deep Right', 'Pass, Deep Middle', 'Pass, Deep Left', 'Pass, Sack', 'Run', 'Run, Right End', 'Run, Right Tackle', 'Run, Right Guard', 'Run, Middle', 'Run, Left Guard', 'Run, Left Tackle', 'Run, Left End', 'Punt', 'Field Goal', 'Run, QB Kneel']
 
       for (i in vm.result) {
-        vm.result[i]
+        var plays = {'name': names.shift(), 'size': vm.result[i]}
 
-        fResult.push({'name': names.shift(), 'size': vm.result[i]})
+        if (plays.name.slice(0, 4) == 'Pass'){
+          fResult[0].children.push(plays);
+        } else if (plays.name.slice(0, 3) == 'Run') {
+          fResult[1].children.push(plays);
+        } else {
+          fResult.push(plays);
+        }
       }
 
       vm.data = [{
@@ -26,8 +36,6 @@ function Main(Prediction, $state) {
       $state.go('main.result');
     })
   }
-
-  console.log(d3);
 
   vm.options = {
     chart: {
