3a7d7b7e676d55a018a8119097d96d89720a00cb
Will Schumacher <meintodengel@gmail.com>
2016-07-05 11:15:03 -0700

    add authorization check for restricted routes

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-angular.module('app', ['ui.router', 'nvd3']);
+angular.module('app', ['ui.router', 'nvd3'])
+  .run(function ($rootScope, $state, Auth) {
+    $rootScope.$on('$stateChangeStart', function (event, next, current) {
+      if (next.restricted && AuthService.isLoggedIn() === false) {
+        $state.go('login');
+      }
+    });
+  });
