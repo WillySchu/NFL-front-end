f160cfe88af618c85d0423d6caa41720883b2641
Will Schumacher <meintodengel@gmail.com>
2016-07-04 19:32:47 -0700

    edited nav to route to login and logout

--- controllers/nav.js
+++ controllers/nav.js
@@ -1,12 +1,21 @@
 angular.module('app')
   .controller('Nav', Nav)
 
-Nav.$inject = [];
+Nav.$inject = ['$state', 'Auth'];
 
-function Nav() {
+function Nav($state, Auth) {
   const vm = this;
 
+  vm.isUser = Auth.isLoggedIn();
+
   vm.signIn = function() {
-    console.log('Sign In!');
+    $state.go('login');
+  }
+
+  vm.logout = function() {
+    Auth.logout().then(function() {
+      nav.isUser = false;
+      $state.go('main');
+    })
   }
 }
